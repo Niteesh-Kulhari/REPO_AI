aae4196e4d5353d67e91553e524f81c2c201abd9
root <root@Niteesh>
2024-12-16 19:36:53 -0500

    Added octokit function

---
+++ src/lib/github.ts
@@ -0,0 +1,40 @@
+import { Octokit } from "octokit";
+
+export const octokit = new Octokit({
+  auth: process.env.GITHUB_TOKEN,
+});
+
+const githubUrl = "https://github.com/Niteesh-Kulhari/Music_SaaS";
+
+type Response = {
+  commitHash: string;
+  commitMessage: string;
+  commitAuthorName: string;
+  commitAuthorAvatar: string;
+  commitDate: string;
+};
+
+export const getCommitHashes = async (
+  githubUrl: string,
+): Promise<Response[]> => {
+  const { data } = await octokit.rest.repos.listCommits({
+    owner: "Niteesh-Kulhari",
+    repo: "Music_SaaS",
+  });
+
+  const sortedCommits = data.sort(
+    (a: any, b: any) =>
+      new Date(b.commit.author.date).getTime() -
+      new Date(a.commit.author.date).getTime(),
+  ) as any[];
+
+  return sortedCommits.slice(0, 15).map((commit: any) => ({
+    commitHash: commit.sha as string,
+    commitMessage: commit.commit.message ?? "",
+    commitAuthorName: commit.commit?.author?.name ?? "",
+    commitAuthorAvatar: commit.commit?.author?.avatar_url ?? "",
+    commitDate: commit.commit?.author?.date ?? "",
+  }));
+};
+
+//console.log(await getCommitHashes(githubUrl));
